a429888ea6cc415e066b60560b286dafeacaec5b
vipulasri <vipulasri.2007@gmail.com>
2020-02-09 17:37:07 +0530

    fix: theme

--- app/src/main/java/com/vipulasri/jetdelivery/ui/Themes.kt
+++ app/src/main/java/com/vipulasri/jetdelivery/ui/Themes.kt
@@ -2,18 +2,12 @@ package com.vipulasri.jetdelivery.ui
 
 import androidx.ui.graphics.Color
 import androidx.ui.material.ColorPalette
+import androidx.ui.material.lightColorPalette
 
-val lightThemeColors = ColorPalette(
+val lightThemeColors = lightColorPalette(
     primary = Color(0xFFFF8C29),
     primaryVariant = Color(0xFFF87D27),
-    onPrimary = Color.White,
     secondary = Color(0xFFFFF2D55),
     secondaryVariant = Color(0xFFFFF0038),
-    onSecondary = Color.White,
-    background = Color.White,
-    onBackground = Color.Black,
-    surface = Color.White,
-    onSurface = Color.Black,
-    error = Color(0xFFD00036),
-    onError = Color.White
+    error = Color(0xFFD00036)
 )
--- app/src/main/java/com/vipulasri/jetdelivery/ui/home/HomeScreen.kt
+++ app/src/main/java/com/vipulasri/jetdelivery/ui/home/HomeScreen.kt
@@ -2,11 +2,13 @@ package com.vipulasri.jetdelivery.ui.home
 
 import androidx.compose.Composable
 import androidx.ui.layout.Column
+import androidx.ui.res.stringResource
+import com.vipulasri.jetdelivery.R
 import com.vipulasri.jetdelivery.ui.AppTopBar
 
 @Composable
 fun HomeScreen() {
     Column {
-        AppTopBar("JetDelivery")
+        AppTopBar(stringResource(id = R.string.app_name))
     }
 }
