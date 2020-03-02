e25c1bac92a090c35f140261b92b5fe41ffc46fa
vipulasri <vipulasri.2007@gmail.com>
2020-03-02 10:18:39 +0530

    update: getcolor from hex in jetpack

--- app/src/main/java/com/vipulasri/jetdelivery/ui/dashboard/Category.kt
+++ app/src/main/java/com/vipulasri/jetdelivery/ui/dashboard/Category.kt
@@ -44,7 +44,7 @@ private fun categoryImage(item: Dashboard.Item.SubItem) {
     Container(width = 70.dp, height = 70.dp) {
         Clip(shape = RoundedCornerShape(5.dp)) {
             val bgColor = item.meta?.bgColor?.let { color ->
-                Color(parseColor(color))
+                getColor(color)
             } ?: Color.Blue
             DrawShape(shape = RectangleShape, color = bgColor)
             Container(width = 40.dp, height = 40.dp) {
@@ -78,14 +78,6 @@ private fun categoryInfo(title: String?, subTitle: String?) {
     }
 }
 
-@ColorInt
-fun parseColor(@Size(min = 1) colorString: String): Int {
-    if (colorString[0] == '#') { // Use a long to avoid rollovers on #ffXXXXXX
-        var color = colorString.substring(1).toLong(16)
-        if (colorString.length == 7) { // Set the alpha value
-            color = color or -0x1000000
-        } else require(colorString.length == 9) { "Unknown color" }
-        return color.toInt()
-    }
-    throw IllegalArgumentException("Unknown color")
+fun getColor(colorString: String): Color {
+    return Color(android.graphics.Color.parseColor(colorString))
 }
